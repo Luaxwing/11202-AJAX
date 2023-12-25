c7d4b36be33cb9d5cdf0b38cf1afbddfa94741a2
Luaxwing <nick890111@gmail.com>
2023-12-25 14:06:48 +0800

    20231225:app2

--- app2/api/get_school.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include_once "db.php";
-$schools=$GraduateSchool->all();
-$options="";
-foreach($schools as $school){
-    $options.="<option value='{$school['id']}'>{$school['county']}{$school['name']}</option>";
-}
-echo $options;
